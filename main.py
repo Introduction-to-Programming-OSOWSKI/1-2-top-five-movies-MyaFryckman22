b9f5d14afd07a408b0a17e3abfbc622e2d93d666
MyaFryckman22 <817258@mystma.org>
2021-09-21 14:43:56 -0500

    3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #WRITE YOUR CODE IN THIS FILE
 #define function 
-def topFiveMovies(a, b, c, d, e):
-    return(a, b, c, d, e)
+def topFiveMovies( a, b, c, d, e):
+    return( a, b, c, d, e)
  
 print(topFiveMovies("Rashamon", "Knives Out", "King of Comedy", "Addams Family", "Darkest Minds"))
